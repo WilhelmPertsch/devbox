f771676c1f99042de91c360e11c4ad00a9e2e9f7
Valentin Despa <info@vdespa.de>
2016-07-24 21:53:04 +0200

    Add devbox.sh for YAD deployments

---
+++ share/etc/yad/t3rookies/typo3/devbox.sh
@@ -0,0 +1,19 @@
+# t3rookies TYPO3 Devbox
+export DB_HOST='localhost'
+export DB_USER='root'
+export DB_PASS='root'
+export DB_NAME='t3rookies_dev'
+export TYPO3_SITENAME='t3rookies - DEVBOX System'
+
+#######################
+# YAD specific settings
+
+export YAD_DEPLOY=simple.sh
+export YAD_RELEASE_FOLDER=/var/www/latest
+export YAD_INSTALL_SCRIPT='setup/install.sh'
+
+# package containing at least an install.sh install script
+# supports tgz and zip packages
+export YAD_PACKAGE=https://t3rookies.random-ip.com/job/t3rookies_build/lastSuccessfulBuild/artifact/package.tar.gz
+export YAD_PACKAGE_USERNAME='package'
+export YAD_PACKAGE_PASSWORD='#############'
