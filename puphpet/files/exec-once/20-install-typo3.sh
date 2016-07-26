0f2cb4807f611726af2ed0786db4e45635facffd
Valentin Despa <valentin.despa@aoe.com>
2016-07-26 12:13:05 +0200

    Set permissions to www

--- puphpet/files/exec-once/20-install-typo3.sh
+++ puphpet/files/exec-once/20-install-typo3.sh
@@ -5,3 +5,6 @@ git clone https://github.com/AOE-T3Rookies-2016/project t3rookies.local
 cd t3rookies.local
 composer update
 ./typo3cms install:setup --non-interactive --database-user-name="root" --database-user-password="root" --database-host-name="localhost" --database-port="3306" --database-name="devbox" --admin-user-name="admin" --admin-password="password" --site-name="t3rookies 2016"
+
+chown -R www-data:www-data /var/www/*
+chmod -R 775 /var/www/*
